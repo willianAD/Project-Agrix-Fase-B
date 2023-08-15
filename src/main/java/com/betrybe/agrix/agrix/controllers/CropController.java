0e1d7d87085b0f23e35d23e9edc02d491ead360b
willianAD <willian.afonso013@gmail.com>
2023-08-15 15:13:53 -0300

    fix: Migrating fase A to B

---
+++ src/main/java/com/betrybe/agrix/agrix/controllers/CropController.java
@@ -0,0 +1,99 @@
+package com.betrybe.agrix.agrix.controllers;
+
+import com.betrybe.agrix.controllers.dto.CropDto;
+import com.betrybe.agrix.models.entities.Crop;
+import com.betrybe.agrix.models.entities.Farm;
+import com.betrybe.agrix.service.CropService;
+import com.betrybe.agrix.service.FarmService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Class CropController.
+ */
+@RestController
+@RequestMapping()
+public class CropController {
+
+  private CropService cropService;
+  private FarmService farmService;
+
+  @Autowired
+  public CropController(CropService cropService, FarmService farmService) {
+    this.cropService = cropService;
+    this.farmService = farmService;
+  }
+
+  /**
+   * Método getAllCrops.
+   */
+  @GetMapping("/crops")
+  public ResponseEntity<?> getAllCrops() {
+    List<Crop> allCrops = cropService.getAllCrops();
+
+    List<CropDto.ToResponse> cropDto = allCrops.stream().map(CropDto::fromEntity).toList();
+    return ResponseEntity.ok(cropDto);
+  }
+
+  /**
+   * Método getCropById.
+   */
+  @GetMapping("/crops/{id}")
+  public ResponseEntity<?> getCropById(@PathVariable Long id) {
+    Optional<Crop> optionalCrop = cropService.getCropById(id);
+    if (optionalCrop.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Plantação não encontrada!");
+    }
+    return optionalCrop.map(crop -> {
+      CropDto.ToResponse response = CropDto.fromEntity(crop);
+      return ResponseEntity.ok(response);
+    }).orElse(ResponseEntity.notFound().build());
+  }
+
+  /**
+   * Método createCrop.
+   */
+  @PostMapping("/farms/{farmId}/crops")
+  public ResponseEntity<?> createCrop(@PathVariable Long farmId, @RequestBody CropDto cropDto) {
+    Optional<Farm> optionalFarm = farmService.getFarmById(farmId);
+    if (optionalFarm.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Fazenda não encontrada!");
+    }
+    return optionalFarm.map(farm -> {
+      Crop crop = cropDto.toCrop();
+      crop.setFarm(farm);
+      Crop savedCrop = cropService.insertCrop(crop);
+      CropDto.ToResponse response = CropDto.fromEntity(savedCrop);
+      return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }).orElse(ResponseEntity.notFound().build());
+  }
+
+  /**
+   * Método getCropFarm.
+   */
+  @GetMapping("/farms/{farmId}/crops")
+  public ResponseEntity<?> getCropFarm(@PathVariable Long farmId) {
+    Optional<Farm> optionalFarm = farmService.getFarmById(farmId);
+    if (optionalFarm.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Fazenda não encontrada!");
+    }
+    return optionalFarm.map(farm -> {
+      List<CropDto.ToResponse> cropResponse =
+          cropService.getCropById(farmId).stream().map(CropDto::froEntity)
+              .collect(Collectors.toList());
+      return ResponseEntity.ok(cropResponse);
+    }).orElse(ResponseEntity.notFound().build());
+  }
+}
---
+++ src/main/java/com/betrybe/agrix/agrix/service/FarmService.java
@@ -0,0 +1,38 @@
+package com.betrybe.agrix.agrix.service;
+
+import com.betrybe.agrix.models.entities.Farm;
+import com.betrybe.agrix.models.repositories.FarmRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Class FarmService.
+ */
+@Service
+public class FarmService {
+  private FarmRepository farmRepository;
+
+  @Autowired
+  public FarmService(FarmRepository farmRepository) {
+    this.farmRepository = farmRepository;
+  }
+
+  //Método insertFarm.
+  public Farm insertFarm(Farm farm) {
+    return farmRepository.save(farm);
+  }
+
+  //Método getAllFarms.
+  public List<Farm> getAllFarms() {
+    return farmRepository.findAll();
+  }
+
+  //Método getFarmById.
+  public Optional<Farm> getFarmById(Long id) {
+    return farmRepository.findById(id);
+  }
+}
